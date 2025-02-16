7972d0928ef22be480b5de630f8fa1747b28a0ec
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-16 12:11:02 +0300

    bonus

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,11 @@
 //part 2
-class Book(var title:String, var author:String, var yearPublished:Int)
+class Book(var title:String, var author:String, var yearPublished:Int, var genre:String)
 
 fun main() {
-    val b1 = Book("Steel Ball Run", "Hirohiko Araki", 2004)
-    print(getBookInfo(b1.title, b1.author, b1.yearPublished))
+    val b1 = Book("Steel Ball Run", "Hirohiko Araki", 2004, "Hard boiled, Fantasy")
+    print(getBookInfo(b1.title, b1.author, b1.yearPublished, b1.genre))
 }
 
-fun getBookInfo(bookTitle:String, bookAuthor:String, bookYearPublished:Int ):String {
-    return "$bookTitle by $bookAuthor ($bookYearPublished)"
+fun getBookInfo(bookTitle:String, bookAuthor:String, bookYearPublished:Int, bookGenre:String ):String {
+    return "$bookTitle by $bookAuthor ($bookYearPublished), genre: $bookGenre"
 }
