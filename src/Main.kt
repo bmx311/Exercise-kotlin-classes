e5cb8dc72ed691d207b0465810a36be00b0be0dd
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-16 11:58:18 +0300

    part 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+//part 1
+class Book {
+    var title = ""
+    var author = ""
+    var yearPublished = 0
+}
+
 fun main() {
-    println("Hello World!")
+    val b1 = Book()
+    b1.title = "Steel Ball Run"
+    b1.author = "Hirohiko Araki"
+    b1.yearPublished = 2004
+
+    println(b1.title)
+    println(b1.author)
+    println(b1.yearPublished)
 }
